739bb70ded89c69428c80599ac10184a065f2d30
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-02-05 20:45:19 +0600

    Update solution.py

--- 633A. Ebony and Ivory/solution.py
+++ 633A. Ebony and Ivory/solution.py
@@ -1,5 +1,3 @@
-import sys
-
 ask = list(map(int, input().split(" ")))
 
 ebony = ask[0]
@@ -7,5 +5,15 @@ ivory = ask[1]
 total_health = ask[2]
 
 
-remaining_health = total_health / ebony
-print(remaining_health)
+ans = False
+for i in range(0, 100):
+    rem = total_health - (ebony * i)
+
+    if rem < 0:
+        break
+
+    if rem % ivory == 0:
+        ans = True
+        break
+
+print("Yes" if ans else "No")
