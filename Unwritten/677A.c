248b4a1e2d83575933c320a324eab886cea0cd48
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-03-12 17:02:51 +0600

    Fuxed

--- Unwritten/677A.c
+++ Unwritten/677A.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
 
 int main()
 {
@@ -8,24 +6,19 @@ int main()
     int n, h=0;
     scanf("%d %d\n", &n, &h);
 
-    char a[n];
-    char ch;
-    int chi=0;
-    while(ch!='\n')    // terminates if user hit enter
-    {
-        ch=getchar();
-        if(ch != " "){
-            a[chi]=ch;
-        }
-        chi++;
-    }
+    int b[n];
 
+    for(int i=0;i<n;i++){
+        scanf(" %d",&b[i]);
+    }
     int width = 0;
 
 
     for (int l = 0; l < n; l++)
     {
-        if (b[l] > h)
+        int num = (int)b[l];
+
+        if (num  > h)
         {
             width = width + 2;
         }
